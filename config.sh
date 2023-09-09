258d6125ab514e6f48610c5e30fffa5cb45c8061
Gubriel <gubriel2017@gmail.com>
2023-09-09 17:58:32 -0300

    add arquivo de configuração

---
+++ config.sh
@@ -0,0 +1,24 @@
+#Requisitos: 
+#   Rode o programa no git bash do seu computador;
+#   Certifique-se de que o script esta rodando na pasta que o programa Docthor-Sys foi apagado.
+
+#Definindo dados
+echo "Digite o nome de usuario do GitHub: >> "
+read user;
+echo "Digite o e-mail do GitHub >> "
+read email;
+echo "Cole o Token de autenticação recebido pelo meu mestre (Gustavo C Pinto) >> "
+read token;
+
+#Configurando usuario e senha no ambinte do git bash
+git config --global user.name  "$user"
+git config --global user.email "$email"
+
+#Configurando credenciais
+git remote set-url origin https://$user:$token@github.com/Gubriel/DocthorSys.git
+
+#Testando credenciais
+echo "script $user" > teste_$user.txt
+git add teste_$user.txt
+git commit -m "teste_$user"
+git push
