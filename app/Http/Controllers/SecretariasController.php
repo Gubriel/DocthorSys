97a6fccda32bf0d247a309e21eebcc617dbd4463
Alissonps15 <alissonportodossantos@gmail.com>
2023-09-18 22:44:03 -0300

    Edição da Tela Secretaria

--- app/Http/Controllers/SecretariasController.php
+++ app/Http/Controllers/SecretariasController.php
@@ -10,7 +10,7 @@ class SecretariasController extends Controller
     public function index()
     {
         $secretarias = array(
-            array('nome' => ' ','especialidade' => ' ','telefone' => ' '),
+            array('nome' => 'Silvana da Silva', 'registro' => '25', 'telefone' => '46991234556', 'email' => 'SilSil@protonmail.com', 'cpf' => '1123456789', 'rg' => '1212315125', 'endereco' => 'Evil-corp'),
         );
 
         return view('app.pages.secretaria.listar', compact('secretarias'));
